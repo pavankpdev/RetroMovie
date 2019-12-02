75066b83b331ce641794a330da29251d62f314bf
pavankpdev <pavankuamrkpkp@gmail.com>
2019-12-02 20:14:54 +0530

    Project rendering file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,53 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  POSTER_SIZE
+} from "../config";
 import HeroImage from "./elements/HeroImage";
 import SearchImage from "./elements/SearchBar";
 import Grid from "./elements/Grid";
 import MovieThumb from "./elements/MovieThumb";
 import Spinner from "./elements/Spinner";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
-
-
-const Home = ()=> (
+import useHomeFetch from "./hooks/useHomeFetch";
+import NoImage from "./images/no_image.jpg";
+const Home = () => {
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log("TCL: Home -> state", state);
+  if (error) return <div>Something went wrong</div>;
+  if (!state.movies[0]) return <Spinner />;
+  return (
     <>
-        <HeroImage/>
-        <SearchImage/>
-        <Grid/>
-        <MovieThumb/>
-        <Spinner/>
-        <LoadMoreBtn/>
+      <HeroImage
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+        title={state.heroImage.original_title}
+        text={state.heroImage.overview}
+      />
+      <SearchImage />
+      <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
+        {state.movies.map(movie => (
+          <MovieThumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                : NoImage
+            }
+            movieId={movie.id}
+            movieName={movie.original_title}
+          />
+        ))}
+      </Grid>
+      <MovieThumb />
+      <Spinner />
+      <LoadMoreBtn />
     </>
-);
-
+  );
+};
 
 export default Home;
