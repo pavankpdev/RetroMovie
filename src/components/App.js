fd08da664b7d5588eed4d377ff55b5384484991e
pavankpdev <pavankuamrkpkp@gmail.com>
2019-12-02 20:15:09 +0530

    react rendering file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Header from "./elements/Header";
 import Home from "./Home";
+import { GlobalStyle } from "../components/styles/GlobalStyle";
+import { StyledSpinner } from "../components/styles/StyledSpinner";
 const App = () => (
   <div>
-    <Header /> <Home/>Start Here!
+    <Header />
+    <Home />
+    <GlobalStyle />
+    <StyledSpinner />
   </div>
 );
 
